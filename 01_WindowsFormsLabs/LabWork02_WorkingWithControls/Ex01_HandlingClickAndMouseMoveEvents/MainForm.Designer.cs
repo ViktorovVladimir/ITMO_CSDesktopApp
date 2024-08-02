b5f727e1c92a7211ff6a6a2bd78a3ee4903adc2d
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-08-03 02:53:45 +0300

    - completed exercise 1 of laboratory work 2
    - completed exercise No. 1: processing the "Click" and "MouseMove" events, laboratory work No. 2: working with controls

---
+++ 01_WindowsFormsLabs/LabWork02_WorkingWithControls/Ex01_HandlingClickAndMouseMoveEvents/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ex01_HandlingClickAndMouseMoveEvents
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnyes = new System.Windows.Forms.Button();
+            this.text = new System.Windows.Forms.Label();
+            this.btnno = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnyes
+            // 
+            this.btnyes.Location = new System.Drawing.Point(28, 49);
+            this.btnyes.Name = "btnyes";
+            this.btnyes.Size = new System.Drawing.Size(128, 35);
+            this.btnyes.TabIndex = 0;
+            this.btnyes.Text = "Yes";
+            this.btnyes.UseVisualStyleBackColor = true;
+            this.btnyes.Click += new System.EventHandler(this.btnyes_Click);
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Location = new System.Drawing.Point(24, 18);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(246, 20);
+            this.text.TabIndex = 1;
+            this.text.Text = "Are you satisfied with your salary?";
+            // 
+            // btnno
+            // 
+            this.btnno.Location = new System.Drawing.Point(28, 91);
+            this.btnno.Name = "btnno";
+            this.btnno.Size = new System.Drawing.Size(128, 35);
+            this.btnno.TabIndex = 2;
+            this.btnno.TabStop = false;
+            this.btnno.Text = "No";
+            this.btnno.UseVisualStyleBackColor = true;
+            this.btnno.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnno_MouseMove);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(473, 140);
+            this.Controls.Add(this.btnno);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.btnyes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "WinQuestion: urgent question";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnyes;
+        private System.Windows.Forms.Label text;
+        private System.Windows.Forms.Button btnno;
+    }
+}
+
---
+++ 01_WindowsFormsLabs/LabWork02_WorkingWithControls/Ex01_HandlingClickAndMouseMoveEvents/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex01_HandlingClickAndMouseMoveEvents
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        //--.
+        private void btnyes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("We have no doubt that you think so!");
+        }
+
+        //--
+        private void btnno_MouseMove(object sender, MouseEventArgs e)
+        {
+            //--.
+            btnno.Top -= e.Y;
+            btnno.Left += e.X;
+            if (btnno.Top < -10 || btnno.Top > 100)
+                btnno.Top = 60;
+            if (btnno.Top < -80 || btnno.Top > 250)
+                btnno.Left = 120;
+
+        }
+    }
+}
