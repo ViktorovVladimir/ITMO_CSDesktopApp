a7eb2c4cc1eb2563f9b2960d9731ac1d76e36ac4
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-08-09 02:53:42 +0300

    - completed laboratory work 8
    Completed laboratory work No. 8 “Data binding”:
    - Exercise No. 1 “Creating data binding to a collection object.”

---
+++ 02_WindowsPresentationFoundationLabs/LabWork08_DataBinding/MyWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ex01_CreatingDataBindingToCollectionObject
+{
+    /// <summary>
+    /// Interaction logic for MyWindow.xaml
+    /// </summary>
+    public partial class MyWindow : Window
+    {
+        //--.
+        private bool _close;
+        //--.
+        MainWindow wnd1 = null;
+
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+        
+        //--.
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            if (wnd1 != null)
+                wnd1.myWin = null;
+        }
+
+        //--.
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //--.
+            if (_close) return;
+            e.Cancel = true;
+            Hide();
+        }
+    }
+}
---
+++ 02_WindowsPresentationFoundationLabs/LabWork08_DataBinding/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_CreatingDataBindingToCollectionObject
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public bool IsEnrolled { get; set; }
+
+        //--.
+        public Student(string name, bool ch)
+        {
+            StudentName = name;
+            IsEnrolled = ch;
+        }
+        
+        //--.
+        public string FullStudentData
+        {
+            get { return StudentName + "\t" + IsEnrolled; }
+        }
+
+        //--. or the same thing, but using lambda expression syntax
+        //public string FullStudentData => StudentName + "\t" + IsEnrolled
+
+    }
+
+
+    //--.
+    public class StudentList : ObservableCollection<Student> 
+    { 
+        public StudentList() 
+        {
+            Add(new Student("Lorin Kanev", true));
+            Add(new Student("Ivan Petrov", false));
+            Add(new Student("Sergey Masov", true));
+            Add(new Student("Tais Frolova", true));
+            Add(new Student("Elena Diva", false));
+        }
+    }
+
+}
