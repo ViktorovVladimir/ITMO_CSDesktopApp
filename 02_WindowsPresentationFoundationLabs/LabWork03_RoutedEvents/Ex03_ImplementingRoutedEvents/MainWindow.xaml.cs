c6b81b0ed64ed63601218a0365053ae185a8a598
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-08-08 23:48:48 +0300

    - completed laboratory work 3
    Completed laboratory work No. 3 “Routed events”:
    - exercise No. 1 “Creating routed event handlers”;
    - exercise No. 2 “Using routed events”;
    - exercise No. 3 “Introducing routed events.”

---
+++ 02_WindowsPresentationFoundationLabs/LabWork03_RoutedEvents/Ex03_ImplementingRoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex03_ImplementingRoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        //--.
+        string nameFile = "username.txt";
+        //--.
+        bool isDataDirty = false;
+
+        //--.
+        public MyWindow myWin { get; set; }
+
+        //--.
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //--.
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+        }
+
+        //--.
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //--.
+            if (this.isDataDirty)
+            {
+                string msg = "Данные были изменены, но не сохранены!\n Закрыть окно без сохранения?";
+                MessageBoxResult result = MessageBox.Show(msg, "Контроль данных", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                //--.
+                if (result == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        //--.
+        private void SetBut()
+        {
+            //--.
+            System.IO.StreamWriter sw = null;
+
+            //--.
+            isDataDirty = false;
+
+            //--.
+            sw = new System.IO.StreamWriter(nameFile);
+            sw.WriteLine(setText.Text);
+            
+            //--.
+            if (sw != null)
+                sw.Close();
+
+            //--.
+            retBut.IsEnabled = true;
+        }
+
+        //--.
+        private void RetBut()
+        {
+            //--.
+            System.IO.StreamReader sr = null;
+
+            //--.
+            using (sr = new System.IO.StreamReader(nameFile))
+                retLabel.Content = "Greetings, dear " + sr.ReadToEnd();
+
+            //--.
+            if (sr != null)
+                sr.Close();
+
+            //--.
+            setBut.IsEnabled = false;
+            retBut.IsEnabled = false;
+        }
+
+        //--.
+        private void setText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //--.
+            setBut.IsEnabled = true;
+
+            //--.
+            isDataDirty = true;
+        }
+
+        //--.
+        private void funcShowNewWindow()
+        {
+            //--.
+            if (myWin == null)
+                myWin = new MyWindow();
+
+
+            //--.
+            var location = New_Win.PointFromScreen(new Point(0, 0));
+            myWin.Left = location.X + New_Win.Width;
+            myWin.Top = location.Y;
+
+
+            //--.
+            myWin.Top = this.Top;
+            myWin.Left = this.Left + this.Width;
+
+            //--.
+            myWin.Owner = this;
+            //--.
+            myWin.Show();
+        }
+
+
+        //--.
+        private void Grid_Click(object sender, RoutedEventArgs e)
+        {   
+            //--.
+            FrameworkElement feSource = e.Source as FrameworkElement;
+
+            //--.
+            try
+            {
+                //--.
+                switch(feSource.Name)
+                {
+                    case "setBut":
+                        SetBut();
+                        break;
+                    case "retBut":
+                        RetBut();
+                        break;
+                    case "New_Win":
+                        funcShowNewWindow();
+                        break;
+                }
+                //--.
+                e.Handled = true;
+            }
+            catch( Exception ex )
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }   
+    }   
+}
